c2145c4337ffe5e4c15ba4e793558d2296b09a1e
mike <mikegwyn17@gmail.com>
2020-04-18 13:43:33 -0400

    egg turns red when it hits the ground

---
+++ LudemDareSpring2020/Assets/EggController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LD
+{
+    public class EggController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.collider.CompareTag(Constants.GROUND_TAG))
+            {
+                // when the egg hits the ground change the collor to red
+                var spriteRenderer = GetComponent<SpriteRenderer>();
+                spriteRenderer.color = Color.red;
+            }
+        }
+    }
+}
