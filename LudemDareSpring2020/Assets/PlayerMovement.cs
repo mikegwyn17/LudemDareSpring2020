f9d621fcfbb94b1dbd392ba556d5fe35e33b56f8
mike <mikegwyn17@gmail.com>
2020-04-18 12:13:18 -0400

    test egg scene

---
+++ LudemDareSpring2020/Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float moveSpeed = 40f;
+    float _horizontalMove = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get values for movement in update but don't aplly them
+        _horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(_horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
