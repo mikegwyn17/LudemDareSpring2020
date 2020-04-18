bebdef43c6273fb1b9dbe7621869c122d3d43d4c
mike <mikegwyn17@gmail.com>
2020-04-18 19:48:07 -0400

    added new tile palette and basic ai

---
+++ LudemDareSpring2020/Assets/EnemyGFX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyGFX : MonoBehaviour
+{
+    public AIPath aiPath;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // based on direction of bird flip the transform
+        // traveling to the right
+        if (aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        // traveling to the left
+        else if (aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f); 
+        }
+    }
+}
