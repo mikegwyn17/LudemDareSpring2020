78676e4968f1c7b7ea1f868294689c44bfbe9842
Joseph Chamberlain <josephlchamberlain@gmail.com>
2020-04-19 03:18:02 -0400

    Added a cave level with parallax background and audio

---
+++ LudemDareSpring2020/Assets/Scripts/Camera/ParallaxBackground.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LD
+{
+
+    public class ParallaxBackground : MonoBehaviour
+    {
+        private Transform cameraTransform;
+        public GameObject player;        //Public variable to store a reference to the player game object
+        private Vector3 lastCameraPosition;
+        [SerializeField] private float parallaxEffectMultiplier;
+
+        private void Start()
+        {
+            cameraTransform = player.transform;
+            lastCameraPosition = cameraTransform.position;
+
+        }
+
+        private void LateUpdate()
+        {
+            Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+
+            transform.position += deltaMovement * parallaxEffectMultiplier;
+            lastCameraPosition = cameraTransform.position;
+        }
+    }
+}
