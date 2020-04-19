004c1434c4c71cf33b4989b828955e21cc4a637c
Joseph Chamberlain <josephlchamberlain@gmail.com>
2020-04-18 20:17:04 -0400

    Fabbed player, added click to move, camera follows player

--- LudemDareSpring2020/Assets/Scripts/Player/PlayerMovement.cs
+++ LudemDareSpring2020/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,34 +2,139 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+namespace LD
 {
-    public CharacterController2D controller;
-    public float moveSpeed = 40f;
-    float _horizontalMove = 0f;
-    bool _isJumping = false;
-
-    // Start is called before the first frame update
-    void Start()
-    { 
-
-    }
-
-    // Update is called once per frame
-    void Update()
+    public class PlayerMovement : MonoBehaviour
     {
-        // get values for movement in update but don't aplly them
-        _horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        public CharacterController2D controller;
+        public float moveSpeed = 40f;
+        float _horizontalMove = 0f;
+        bool _isJumping = false;
+        State playerState = State.Normal;
 
-        if (Input.GetButton("Jump"))
+        //For swinging
+        private Vector3 spitTargetPosition;
+        private Vector3 dirToTarget;
+        private float spitTravelSpeed = 20f;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            _isJumping = true;
-        }
-    }
 
-    private void FixedUpdate()
-    {
-        controller.Move(_horizontalMove * Time.fixedDeltaTime, false, _isJumping);
-        _isJumping = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            switch (playerState)
+            {
+                case State.Normal:
+                    CheckForJumping();
+                    CheckForSpitSwingStart();
+                    break;
+                case State.Jumping:
+                    break;
+                case State.SpitSwinging:
+                    break;
+                case State.SpitSwingStart:
+                    break;
+                case State.SpitSwingTravel:
+                    break;
+            }
+
+            // get values for movement in update but don't apply them
+            _horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+            
+        }
+
+        private void FixedUpdate()
+        {
+            switch (playerState)
+            {
+                case State.Normal:
+                    break;
+                case State.Jumping:
+                    break;
+                case State.SpitSwinging:
+                    HandleSpitSwinging();
+                    break;
+                case State.SpitSwingStart:
+                    break;
+                case State.SpitSwingTravel:
+                    HandleSpitSwingTravel();
+                    break;
+            }
+
+            controller.Move(_horizontalMove * Time.fixedDeltaTime, false, _isJumping);
+            _isJumping = false;
+
+            if(playerState != State.SpitSwinging)
+            {
+                playerState = State.Normal;
+            }
+            
+        }
+
+        private void CheckForJumping()
+        {
+            if (Input.GetButton("Jump"))
+            {
+                _isJumping = true;
+                playerState = State.Jumping;
+            }
+        }
+        
+
+        private void CheckForSpitSwingStart()
+        {
+            if (playerState != State.SpitSwinging && playerState != State.SpitSwingTravel && Input.GetMouseButtonDown(0))
+            {
+                SpitSwing ss = new SpitSwing();
+                spitTargetPosition = GetMouseWorldPosition();
+                dirToTarget = (spitTargetPosition - GetPosition()).normalized;
+                spitTravelSpeed = 20f;
+                playerState = State.SpitSwinging;
+            }
+
+        }
+
+        private void HandleSpitSwinging()
+        {
+            transform.position += dirToTarget * Time.deltaTime * spitTravelSpeed;
+            if (Vector3.Distance(GetPosition(), spitTargetPosition) < 2f)
+            {
+                playerState = State.SpitSwingTravel;
+            }
+        }
+
+        private void HandleSpitSwingTravel()
+        {
+            spitTravelSpeed -= spitTravelSpeed * Time.deltaTime;
+            transform.position += dirToTarget * Time.deltaTime * spitTravelSpeed;
+            if(spitTravelSpeed < 0.5f)
+            {
+                playerState = State.Normal;
+            }
+        }
+
+        // Get Mouse Position in World with Z = 0f
+        public static Vector3 GetMouseWorldPosition()
+        {
+            Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+            vec.z = 0f;
+            return vec;
+        }
+
+        public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+        {
+            Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+            return worldPosition;
+        }
+
+        public Vector3 GetPosition()
+        {
+            return transform.position;
+        }
     }
 }
